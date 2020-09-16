bc3b7887af0eba07f94ecb0087a0321f48369982
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-16 11:41:10 +0200

    init project with jest configuration for testing

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    preset: '@vue/cli-plugin-unit-jest',
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+    },
+    moduleFileExtensions: [
+        'js',
+        'json',
+        // tell Jest to handle `*.vue` files
+        'vue',
+    ],
+};
