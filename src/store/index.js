b9ed2e89222796657da30512638ef77bf35042fa
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-17 18:50:17 +0200

    throwaway branch, trying out the new composition api and latest vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,17 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        count: 0,
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        decrement(state) {
+            state.count--;
+        },
+    },
+    actions: {},
+    modules: {},
+});
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="vuexExample">
+      <h2>Count: {{ count }}</h2>
+      <button @click="increment()">Increment</button>
+      <button @click="decrement()">Decrement</button>
+    </div>
   </div>
 </template>
+
+<script>
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+export default {
+  setup() {
+    const store = useStore();
+    const count = computed(() => store.state.count);
+
+    const increment = () => {
+      store.commit("increment");
+    };
+
+    const decrement = () => {
+      store.commit("decrement");
+    };
+
+    return {
+      count,
+      increment,
+      decrement,
+    };
+  },
+};
+</script>
