10fa7c1e9bf169755ce8f35ab1e55c8dfe168179
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-21 11:30:38 +0200

    add basic styling based on figma sketch linked in the project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Raleway", sans-serif;
+  background-color: #4b0303;
+  color: #feecee;
 }
 
+h1,
+h2,
+h3,
+h4,
+h5 {
+  font-family: "Loved by the King", cursive;
+}
 #nav {
   padding: 30px;
 }
