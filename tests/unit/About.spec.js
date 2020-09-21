485b13fb239852f8d1b6e37e7a3318dedb6bbb8c
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-21 13:14:50 +0200

    add basic styling

--- tests/unit/About.spec.js
+++ tests/unit/About.spec.js
@@ -11,4 +11,19 @@ describe('About.vue', () => {
         let element = wrapper.find('p');
         expect(element.exists()).toBe(true);
     });
+
+    it('should contain an h2', () => {
+        let element = wrapper.find('h2');
+        expect(element.exists()).toBe(true);
+    });
+
+    it('should contain an h3', () => {
+        let element = wrapper.find('h3');
+        expect(element.exists()).toBe(true);
+    });
+
+    it('should contain an image', () => {
+        let element = wrapper.find('img');
+        expect(element.exists()).toBe(true);
+    });
 });
