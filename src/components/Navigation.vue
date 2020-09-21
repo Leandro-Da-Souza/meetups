b897103c412319690bb213e1fdb4e313f74cab98
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-21 12:26:37 +0200

    add some styling to navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,9 +3,10 @@
     <div class="left">
       <h1>Eventful</h1>
     </div>
-    <div class="right"></div>
-    <button>My Events</button>
-    <button>About</button>
+    <div class="right">
+      <button>My Events</button>
+      <button>About</button>
+    </div>
   </div>
 </template>
 
@@ -15,5 +16,24 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.container {
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 15px;
+}
+
+button {
+  font-size: 1rem;
+  border: none;
+  margin-left: 10px;
+  padding-top: 1.2rem;
+  outline: none;
+}
+
+button:hover {
+  border-bottom: 1px solid;
+  font-size: 1.1rem;
+}
 </style>
