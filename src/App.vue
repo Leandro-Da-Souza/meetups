13b49295a79262081da4a8091bf5eaca2bfe9b8d
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-22 10:36:56 +0200

    trying to figure out the createLocalVue problem caused the a whole new mess of problems, now App.spec.js fails at mount though the test initially passed, says that importing App returns undefined, the other tests runs fine

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
-  <div id="nav">
-    <Navigation />
-  </div>
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component"></component>
-    </transition>
-  </router-view>
+    <div id="nav">
+        <Navigation />
+    </div>
+    <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+            <component :is="Component"></component>
+        </transition>
+    </router-view>
 </template>
 
 <script>
-import Navigation from "./components/Navigation";
+import Navigation from './components/Navigation';
 
 export default {
-  name: "App",
-  components: {
-    Navigation,
-  },
+    name: 'App',
+    components: {
+        Navigation,
+    },
 };
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: "Raleway", sans-serif;
-  background-color: #4b0303;
-  color: #feecee;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Raleway', sans-serif;
+    background-color: #4b0303;
+    color: #feecee;
 }
 
 h1,
@@ -35,14 +35,14 @@ h2,
 h3,
 h4,
 h5 {
-  font-family: "Loved by the King", cursive;
+    font-family: 'Loved by the King', cursive;
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.5s;
+    transition: opacity 0.5s;
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
+    opacity: 0;
 }
 </style>
