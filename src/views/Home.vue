e1ace1b48983192dfa63530802a98b1e91355c50
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-24 11:50:19 +0200

    manage to fix vuex and render data home template, will discard jsonserver since i can just get my data directly from the file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
     <div class="home">
-        Hello
+        <ul>
+            <li v-for="(event, index) in pastEvents" :key="index">
+                {{ event.category }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import * as DB from '../../db.json';
 // @ is an alias to /src
 
 export default {
     name: 'Home',
     components: {},
+    setup() {
+        const store = useStore();
+
+        store.commit('SET_COMING_EVENTS', DB.events[1]);
+        store.commit('SET_PAST_EVENTS', DB.events[0]);
+
+        const comingEvents = computed(
+            () => store.state.comingEvents.comingEvents
+        );
+        const pastEvents = computed(() => store.state.pastEvents.pastEvents);
+
+        return {
+            comingEvents,
+            pastEvents,
+        };
+    },
 };
 </script>
