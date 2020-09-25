0504857126200d20e6bc04b7f62b1c95af0fa6e5
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-25 07:55:29 +0200

    change data structure so that app.vue sets the comingEvents and pastEvents in store

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,50 @@
 </template>
 
 <script>
+// import { computed } from 'vue';
+import { useStore } from 'vuex';
+import { v4 as uuidv4 } from 'uuid';
+import * as DB from '../db.json';
+
 import Navigation from './components/Navigation';
 
 export default {
     name: 'App',
     components: {
         Navigation,
+    },
+    setup() {
+        const store = useStore();
+
+        const dbComingEvents = DB.events[1].comingEvents;
+        const dbPastEvents = DB.events[0].pastEvents;
+
+        const getRandomPic = (event) => {
+            return (event.picture = `http://placeimg.com/100/100/${event.category}`);
+        };
+
+        dbComingEvents.forEach((event) => {
+            event.id = uuidv4();
+            event.attending = false;
+            getRandomPic(event);
+        });
+
+        dbPastEvents.forEach((event) => {
+            event.id = uuidv4();
+            event.attended = false;
+            getRandomPic(event);
+        });
+
+        store.commit('SET_COMING_EVENTS', dbComingEvents);
+        store.commit('SET_PAST_EVENTS', dbPastEvents);
+
+        // const comingEvents = computed(() => store.state.comingEvents);
+        // const pastEvents = computed(() => store.state.pastEvents);
+
+        // console.log(comingEvents.value);
+        // pastEvents.value.forEach((event) => {
+        //     event.id = uuidv4();
+        // });
     },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,8 +12,6 @@
 <script>
 import { computed } from 'vue';
 import { useStore } from 'vuex';
-import { v4 as uuidv4 } from 'uuid';
-import * as DB from '../../db.json';
 // @ is an alias to /src
 
 export default {
@@ -21,28 +19,6 @@ export default {
     components: {},
     setup() {
         const store = useStore();
-
-        const dbComingEvents = DB.events[1].comingEvents;
-        const dbPastEvents = DB.events[0].pastEvents;
-
-        const getRandomPic = (event) => {
-            return (event.picture = `http://placeimg.com/100/100/${event.category}`);
-        };
-
-        dbComingEvents.forEach((event) => {
-            event.id = uuidv4();
-            event.attending = false;
-            getRandomPic(event);
-        });
-
-        dbPastEvents.forEach((event) => {
-            event.id = uuidv4();
-            event.attended = false;
-            getRandomPic(event);
-        });
-
-        store.commit('SET_COMING_EVENTS', dbComingEvents);
-        store.commit('SET_PAST_EVENTS', dbPastEvents);
 
         const comingEvents = computed(() => store.state.comingEvents);
         const pastEvents = computed(() => store.state.pastEvents);
