958888957b182771be8668497d0764df3d8fb0bd
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-09-30 15:32:41 +0200

    fix error with routeChange in deployment and fixed cypress test

--- tests/e2e/specs/Event.js
+++ tests/e2e/specs/Event.js
@@ -1,10 +1,10 @@
 describe('Testing event functionality', () => {
-    it('visits the individual event page for first event', () => {
+    it('visits the individual event page for first item when clicked', () => {
         cy.visit('/');
         cy.get('.home ul li')
             .first()
             .trigger('mouseover')
             .click({ force: true });
-        cy.url().should('include', 'event/0');
+        cy.url().should('include', 'event/4');
     });
 });
